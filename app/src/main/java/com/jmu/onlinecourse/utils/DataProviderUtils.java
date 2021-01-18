57b1ac11fe5e94bac514c8a42acf5e12b7db1da5
Vanish-Zeng <957589005@qq.com>
2021-01-19 01:36:33 +0800

    视频素材更新

--- app/src/main/java/com/jmu/onlinecourse/utils/DataProviderUtils.java
+++ app/src/main/java/com/jmu/onlinecourse/utils/DataProviderUtils.java
@@ -21,21 +21,21 @@ public class DataProviderUtils {
         videoInfo1.setVideoName("秋收起义");
         videoInfo1.setSummary("1927年，蒋介石背叛革命，一片白色恐怖弥漫在华夏大地上空。中国共产党于8月7日召开了紧急会议，委派毛泽东前往湖南组织秋收武装暴动。毛泽东决定成立工农革命军第一军第一师，并将暴动时间定为1927年9月9日。毛泽东和潘心源、陈志安走到张家坊，却被阎仲甫所设的哨卡拦住。几个团丁押着三人走在山路上，毛泽东巧施妙计得以脱身，三人来到铜鼓镇工农革命军第三团集结地。秋收起义终于打响了。工农革命军一路攻下多处要害后遭到敌人包围，因敌强我弱，部队损失惨重。毛泽东仔细思考局势，在会上指出敌我双方实力悬殊，我方应先站稳脚跟，保存实力。随后，毛泽东带领部队前往敌人统治力量最为薄弱的罗霄山脉，建立苏维埃政权。部队终于冲破敌人的围剿，毛泽东在几百名战士的簇拥下明确了拉着队伍上井冈的部署，坚定地表示必将打倒蒋介石。");
         videoInfo1.setImageUrl("https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2371889280.webp");
-        videoInfo1.setDetailUrl("https://v-cdn.zjol.com.cn/283323.mp4");
+        videoInfo1.setDetailUrl("https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/aedd12c4-4b16-4e8c-b88e-7944d32e1516.mp4");
         videoInfo1.setLikes(100);
 
         VideoInfo videoInfo2 = new VideoInfo();
         videoInfo2.setVideoName("小平您好");
         videoInfo2.setSummary("为了向邓小平同志诞辰100周年献礼，由中央文献研究室、中央电视台、中央新闻纪录电影制片厂、江苏省广播电视总台联合摄制的文献纪录片《小平您好》以邓小平的经历和历史风云为背景，通过细节来叙述他的个性风采和情感世界。编创人员从半个多世纪积累的数万尺胶片中，披沙拣金，撷取大量珍贵的镜头，让观众体会口传故事中无法体会的历史真切，感受一代伟人的智慧胆识，聆听老人发自肺腑的心声，品味领袖生活的朴实无华。");
         videoInfo2.setImageUrl("https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2507753120.webp");
-        videoInfo2.setDetailUrl("https://v-cdn.zjol.com.cn/234572.mp4");
+        videoInfo2.setDetailUrl("https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/2bf2960d-5258-48ba-ac3f-c7b95006da5b.mp4");
 
         VideoInfo videoInfo3 = new VideoInfo();
         videoInfo3.setVideoName("东方红");
         videoInfo3.setSummary("  电影《东方红》的拍摄，对1964年的舞台演出版本进行了必要的删减。把舞台剧原有的8场，只选取了序幕“东方红”和前6场的“东方的曙光”、“星火燎原”、“万水千山”、“抗日的烽火”、“埋葬蒋家王朝”、“中国人民站起来”，而后两场的“祖国在前进”和“世界在前进”，则遵照毛泽东的意见，没有收入到电影画面中去。\n" +
                 "  在拍摄演出过程时，大部分画面采用多机位拍摄，而舞台上的许多集体亮相和群众演员的造型，则采用舞台全景和台下观众相互融合在一个画面的广角镜头，以显示出舞台演出的盛大规模，同时，也是把周恩来所反复强调的“不要离开舞台”的指示，很好地进行了艺术化的处理，从而使整部电影画面流动，情景交融，而不是机械式地一个机位固定拍摄。所以，拍摄完成的舞台艺术纪录片，既丝毫没有脱离舞台，又大胆创新，把舞台演出和艺术纪录完美地融合在一起，为今后同类样式的电影拍摄，开辟了一条崭新的道路。");
         videoInfo3.setImageUrl("https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2329410348.webp");
-        videoInfo3.setDetailUrl("https://v-cdn.zjol.com.cn/323452.mp4");
+        videoInfo3.setDetailUrl("https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/d472a1a6-7faa-46fc-8819-6ddd147e660a.mp4");
 
         VideoInfo videoInfo4 = new VideoInfo();
         videoInfo4.setVideoName("东方红——音乐舞蹈史诗");
@@ -43,43 +43,50 @@ public class DataProviderUtils {
                 "　　1964年，音乐舞蹈史诗《东方红》轰动了全国，其规模之宏大、水平之高、影响之广泛，是史无前例的。\n" +
                 "　　这部鸿篇巨制是在周恩来总理的倡导下进行的，以歌、舞、诗三位一体的形式，描写了中国人民从苦难走向胜利的艰苦历程。这部作品以严谨的结构、宏伟的气势、精巧的设计和高超的技艺来展现主题，根据主题发展的需要，穿插了大量的新编作品、各个历史时期在群众中广为流传的优秀歌曲、建国以来的优秀舞蹈曲目等等。");
         videoInfo4.setImageUrl("https://img9.doubanio.com/view/subject/m/public/s2766474.jpg");
-        videoInfo4.setDetailUrl("https://v-cdn.zjol.com.cn/323452.mp4");
+        videoInfo4.setDetailUrl("https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/4880a114-b71c-4ebd-ac58-2e3be423be1e.mp4");
 
         VideoInfo videoInfo5 = new VideoInfo();
         videoInfo5.setVideoName("永远的小平");
         videoInfo5.setSummary("电视专题片《永远的小平》由凤凰卫视摄制，以邓小平的家人、身边工作人员作为主要的采访对象，其夫人、儿女、妹妹等面对镜头，讲述了他们心目中的邓小平。其中，邓小平与夫人卓琳心心相映、患难与共、相伴走过58个风云多变的春夏秋冬的故事尤为感人。");
         videoInfo5.setImageUrl("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3347870091,3555500830&fm=26&gp=0.jpg");
-        videoInfo5.setDetailUrl("http://v-cdn.zjol.com.cn/323111.mp4");
+        videoInfo4.setDetailUrl("https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/4880a114-b71c-4ebd-ac58-2e3be423be1e.mp4");
 
         VideoInfo videoInfo6 = new VideoInfo();
-        videoInfo6.setVideoName("666");
+        videoInfo6.setVideoName("永远的小平1");
         videoInfo6.setSummary("电视专题片《永远的小平》由凤凰卫视摄制，以邓小平的家人、身边工作人员作为主要的采访对象，其夫人、儿女、妹妹等面对镜头，讲述了他们心目中的邓小平。其中，邓小平与夫人卓琳心心相映、患难与共、相伴走过58个风云多变的春夏秋冬的故事尤为感人。");
         videoInfo6.setImageUrl("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3347870091,3555500830&fm=26&gp=0.jpg");
+        videoInfo4.setDetailUrl("https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/4880a114-b71c-4ebd-ac58-2e3be423be1e.mp4");
+
 
         VideoInfo videoInfo7 = new VideoInfo();
-        videoInfo7.setVideoName("777");
+        videoInfo7.setVideoName("永远的小平2");
         videoInfo7.setSummary("电视专题片《永远的小平》由凤凰卫视摄制，以邓小平的家人、身边工作人员作为主要的采访对象，其夫人、儿女、妹妹等面对镜头，讲述了他们心目中的邓小平。其中，邓小平与夫人卓琳心心相映、患难与共、相伴走过58个风云多变的春夏秋冬的故事尤为感人。");
         videoInfo7.setImageUrl("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3347870091,3555500830&fm=26&gp=0.jpg");
+        videoInfo4.setDetailUrl("https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/4880a114-b71c-4ebd-ac58-2e3be423be1e.mp4");
 
         VideoInfo videoInfo8 = new VideoInfo();
-        videoInfo8.setVideoName("888");
+        videoInfo8.setVideoName("永远的小平3");
         videoInfo8.setSummary("电视专题片《永远的小平》由凤凰卫视摄制，以邓小平的家人、身边工作人员作为主要的采访对象，其夫人、儿女、妹妹等面对镜头，讲述了他们心目中的邓小平。其中，邓小平与夫人卓琳心心相映、患难与共、相伴走过58个风云多变的春夏秋冬的故事尤为感人。");
         videoInfo8.setImageUrl("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3347870091,3555500830&fm=26&gp=0.jpg");
+        videoInfo4.setDetailUrl("https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/4880a114-b71c-4ebd-ac58-2e3be423be1e.mp4");
 
         VideoInfo videoInfo9 = new VideoInfo();
-        videoInfo9.setVideoName("999");
+        videoInfo9.setVideoName("永远的小平4");
         videoInfo9.setSummary("电视专题片《永远的小平》由凤凰卫视摄制，以邓小平的家人、身边工作人员作为主要的采访对象，其夫人、儿女、妹妹等面对镜头，讲述了他们心目中的邓小平。其中，邓小平与夫人卓琳心心相映、患难与共、相伴走过58个风云多变的春夏秋冬的故事尤为感人。");
         videoInfo9.setImageUrl("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3347870091,3555500830&fm=26&gp=0.jpg");
+        videoInfo4.setDetailUrl("https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/4880a114-b71c-4ebd-ac58-2e3be423be1e.mp4");
 
         VideoInfo videoInfo10 = new VideoInfo();
-        videoInfo10.setVideoName("10");
+        videoInfo10.setVideoName("永远的小平5");
         videoInfo10.setSummary("电视专题片《永远的小平》由凤凰卫视摄制，以邓小平的家人、身边工作人员作为主要的采访对象，其夫人、儿女、妹妹等面对镜头，讲述了他们心目中的邓小平。其中，邓小平与夫人卓琳心心相映、患难与共、相伴走过58个风云多变的春夏秋冬的故事尤为感人。");
         videoInfo10.setImageUrl("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3347870091,3555500830&fm=26&gp=0.jpg");
+        videoInfo4.setDetailUrl("https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/4880a114-b71c-4ebd-ac58-2e3be423be1e.mp4");
 
         VideoInfo videoInfo11 = new VideoInfo();
-        videoInfo11.setVideoName("11");
+        videoInfo11.setVideoName("永远的小平6");
         videoInfo11.setSummary("电视专题片《永远的小平》由凤凰卫视摄制，以邓小平的家人、身边工作人员作为主要的采访对象，其夫人、儿女、妹妹等面对镜头，讲述了他们心目中的邓小平。其中，邓小平与夫人卓琳心心相映、患难与共、相伴走过58个风云多变的春夏秋冬的故事尤为感人。");
         videoInfo11.setImageUrl("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3347870091,3555500830&fm=26&gp=0.jpg");
+        videoInfo4.setDetailUrl("https://vkceyugu.cdn.bspapp.com/VKCEYUGU-imgbed/4880a114-b71c-4ebd-ac58-2e3be423be1e.mp4");
 
         list.add(videoInfo1);
         list.add(videoInfo2);
